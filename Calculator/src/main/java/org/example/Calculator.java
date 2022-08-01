ec7c5a6235e25b8f02f23e062fd6a84a869c3f36
vytautasdev <vytautasdev@gmail.com>
2022-08-01 14:25:17 +0100

    First commit

--- Calculator/src/main/java/org/example/Calculator.java
+++ Calculator/src/main/java/org/example/Calculator.java
@@ -6,38 +6,38 @@ public class Calculator {
     int x;
     int y;
 
-    public int plus() {
+    public int add() {
         var scn = new Scanner(System.in);
-        System.out.println("Enter first no.");
+        System.out.println("Enter first number: ");
         x = scn.nextInt();
-        System.out.println("Enter second number");
+        System.out.println("Enter second number: ");
         y = scn.nextInt();
         return x + y;
     }
 
-    public int minus() {
+    public int subtract() {
         var scn = new Scanner(System.in);
-        System.out.println("Enter first no.");
+        System.out.println("Enter first number: ");
         x = scn.nextInt();
-        System.out.println("Enter second number");
+        System.out.println("Enter second number: ");
         y = scn.nextInt();
         return x - y;
     }
 
-    public int division() {
+    public int divide() {
         var scn = new Scanner(System.in);
-        System.out.println("Enter first no.");
+        System.out.println("Enter first number: ");
         x = scn.nextInt();
-        System.out.println("Enter second number");
+        System.out.println("Enter second number: ");
         y = scn.nextInt();
         return x / y;
     }
 
     public int multiply() {
         var scn = new Scanner(System.in);
-        System.out.println("Enter first no.");
+        System.out.println("Enter first number: ");
         x = scn.nextInt();
-        System.out.println("Enter second number");
+        System.out.println("Enter second number: ");
         y = scn.nextInt();
         return x * y;
     }
--- Calculator/src/main/java/org/example/Main.java
+++ Calculator/src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var calculator = new Calculator();
+        var scn = new Scanner(System.in);
+
+        System.out.println("Hello welcome to my calculator");
+        System.out.println("------------------------------");
+        System.out.println("What would you like to do? please enter a number");
+        System.out.println("1.Addition");
+        System.out.println("2.Subtraction");
+        System.out.println("3.Division");
+        System.out.println("4.Multiplication");
+        var choice = scn.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("You have selected - Addition operation.");
+                System.out.println("The result is: " + calculator.add());
+                break;
+            case 2:
+                System.out.println("You have selected - Subtraction operation.");
+                System.out.println("The result is: " + calculator.subtract());
+                break;
+            case 3:
+                System.out.println("You have selected - Division operation.");
+                System.out.println("The result is: " + calculator.divide());
+                break;
+            case 4:
+                System.out.println("You have selected - Multiplication operation.");
+                System.out.println("The result is: " + calculator.multiply());
+                break;
+            default:
+                System.out.println("I know only so much mathematics.. please enter a valid choice");
+
+        }
     }
 }
